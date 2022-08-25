d6215110096d0aaabbfe45f89f1f7299ce7e259a
jcugnoni-heig <99403706+jcugnoni-heig@users.noreply.github.com>
2022-08-25 10:19:07 +0200

    Update installDependencies.sh

--- src/installDependencies.sh
+++ src/installDependencies.sh
@@ -1,1 +1,3 @@
-apt install python python-pyqt5 python-numpy python3 python3-dev python3-numpy tk bison flex liblapack-dev libboost-numpy-dev zlib1g-dev libgfortran3 net-tools libopenblas-base 
+apt install gcc gfortran g++ cmake python python-pyqt5 python-numpy git gcc python3 python3-dev python3-numpy tk bison flex liblapack-dev libboost-numpy-dev zlib1g-dev libgfortran3 net-tools libopenblas-base libgtk2.0-0 libnlopt0 libqt5x11extras5 libqt5opengl5 libglu1-mesa 
+
+
