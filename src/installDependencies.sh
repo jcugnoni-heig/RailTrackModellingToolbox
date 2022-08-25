bf75415f5b2e2c1b8227718d0f970b3f9fd05a49
jcugnoni-heig <99403706+jcugnoni-heig@users.noreply.github.com>
2022-08-25 15:08:05 +0200

    Update installDependencies.sh
    add english language pack to fix Python not supporting UTF8 files

--- src/installDependencies.sh
+++ src/installDependencies.sh
@@ -1,3 +1,3 @@
-apt install gcc gfortran g++ cmake python python-pyqt5 python-numpy git gcc python3 python3-dev python3-numpy tk bison flex liblapack-dev libboost-numpy-dev zlib1g-dev libgfortran3 net-tools libopenblas-base libgtk2.0-0 libnlopt0 libqt5x11extras5 libqt5opengl5 libglu1-mesa 
+apt install language-pack-en gcc gfortran g++ cmake python python-pyqt5 python-numpy git gcc python3 python3-dev python3-numpy tk bison flex liblapack-dev libboost-numpy-dev zlib1g-dev libgfortran3 net-tools libopenblas-base libgtk2.0-0 libnlopt0 libqt5x11extras5 libqt5opengl5 libglu1-mesa 
 
 
