9b14adbeeeacb5460c9e8eeb4d73817e76eaf8f8
jcugnoni-heig <99403706+jcugnoni-heig@users.noreply.github.com>
2022-08-25 09:12:41 +0200

    Update changePath.sh
    auto path detect

--- src/ImpulseModel/working_directory/changePath.sh
+++ src/ImpulseModel/working_directory/changePath.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+SCRIPT=$(realpath "$0")
+# change paths to match current installation
 
-sourcePath=/home/cae/Documents/Railpad2/ImpulseAster_GUI/impulse3sleeper
-destPath=/home/cae/Documents/Railpad2/ImpulseAster_GUI/working_directory
+#detect previous path from .export files 
+src=$(grep "F comm" *.export | head -1 | cut -d " " -f 3)
+sourcePath=${src%/*}
 
-sed -i "s#$sourcePath#$destPath#g" *.astk
-sed -i "s#$sourcePath#$destPath#g" *.export
+destPath=${SCRIPT%/*}
+
+echo "************"
+echo "Changing all paths in .export & .astk files from :"
+echo $sourcePath
+echo "to"
+echo $destPath
+echo "************"
 
 
+read -p "Do you want to proceed? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo ok, we will proceed;
+             sed -i "s#$sourcePath#$destPath#g" *.astk
+             sed -i "s#$sourcePath#$destPath#g" *.export
+             exit;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
