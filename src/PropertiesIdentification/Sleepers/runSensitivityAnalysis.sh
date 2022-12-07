d8dcabfee5d72a5cf9f5011940085d875c20cdb2
maurice <maurice.ammann@heig-vd.ch>
2022-12-07 10:14:39 +0100

    First version of the properties identification scripts.
    This first version of the sleeper properties identifications take as inputs the experimental eigen frequencies and an initial set of properties. It first computes the sensitivity matrix and then loop through the properties to match the numerical eigenfrequencies with the experimental ones.

--- src/PropertiesIdentification/Sleepers/runSensitivityAnalysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run Phase 1 of Harmonic model: generation of model with ties 
-
-## INPUT PARAMETERS
-ASRUNPATH=/opt/SalomeMeca/V2019_univ/tools/Code_aster_frontend-20190/bin/as_run
-ASRUNJOB="sensitivityAnalysis.export"
-### run ASTER to generate mesh to MED format 
-$ASRUNPATH $ASRUNJOB
